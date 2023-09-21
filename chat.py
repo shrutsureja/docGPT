88848c6465a786d3ae76dbbd3ec2a10e01f1441e
Shrut Sureja <92169549+shrutsureja@users.noreply.github.com>
2023-09-21 17:11:01 +0530

    made chat.py for kk

--- chat.py
+++ chat.py
@@ -2,18 +2,12 @@ import os
 from dataclasses import asdict, dataclass
 
 from dotenv import load_dotenv
-from langchain.llms import LlamaCpp
+# from langchain.llms import LlamaCpp
 from langchain import PromptTemplate, LLMChain
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-# from utils import format_prompt
-
-if not load_dotenv():
-    print("Could not load .env file or it is empty. Please check if it exists and is readable.")
-    exit(1)
-
-model_path = os.environ.get("MODEL_PATH")
+from llama_cpp import Llama
 
 template = """Question: {question}
 
@@ -21,59 +15,15 @@ Answer: Let's work this out in a step by step way to be sure we have the right a
 
 prompt = PromptTemplate(template=template, input_variables=["question"])
 
-
-# Callbacks support token-wise streaming
-callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-n_gpu_layers = 40  # Change this value based on your model and your GPU VRAM pool.
-n_batch = 512  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
-
-
-n_gpu_layers=12,
-n_threads=4,
-n_batch=1000
-
-# @dataclass
-# class GenerationConfig:
-#     # sample
-#     top_k: int
-#     top_p: float
-#     temperature: float
-#     repetition_penalty: float
-#     last_n_tokens: int
-#     seed: int
-
-#     # eval
-#     batch_size: int
-#     threads: int
-
-#     # generate
-#     max_new_tokens: int
-#     stop: list[str]
-#     stream: bool
-#     reset: bool
-
-
 def load_model():
     try:
         # check if the model is already downloaded
         if os.path.exists(model_path):
             print("Loading model...")
             global llm
-            # llm = CTransformers(
-            #     model=os.path.abspath(model_path),
-            #     model_type="mpt",
-            #     callbacks=[StreamingStdOutCallbackHandler()],
-            # )
-            # Make sure the model path is correct for your system!
-            llm = LlamaCpp(
+            llm = Llama(
                 model_path=model_path,
-                # temperature=0.75,
-                max_tokens=2000,
-                n_ctx=512,
-                # top_p=1,
-                callback_manager=callback_manager, 
-                verbose=True, # Verbose is required to pass to the callback manager
-            )
+                )
             return True
         else:
             raise ValueError(
@@ -107,7 +57,11 @@ if __name__ == "__main__":
     while True:
         query = input("\nEnter a question: ")
         # query = "Question: " + query
-        # if query == "Question: exit":
+        # if query == "Question: exit":ut
+        input = """A chat between a curious human ("kunjan") and an artificial intelligence assistant ("3K"). The assistant gives helpful, detailed, and polite answers to the human's questions.my name is 3K.the problem statement name is harharmahadev.
+                    kunjan: Hello, 3K.
+                    3K: Hello. How may I help you today?
+                    kunjan:"""
         if query == "exit":
             break
         if query.strip() == "":
@@ -116,8 +70,8 @@ if __name__ == "__main__":
             print("Thinking...")
             # call llm with formatted user prompt and generation config
             # response = llm(format_prompt(query), **apredict(generation_config))
-            llm_chain = LLMChain(prompt=prompt, llm=llm)
-            llm_chain.run(query)
+            # llm_chain = LLMChain(prompt=prompt, llm=llm)
+            llm_chain.run(input + query)
             # response = llm(query)
             # print response
             print("\n")
